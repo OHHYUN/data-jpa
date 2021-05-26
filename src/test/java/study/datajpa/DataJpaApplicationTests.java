dc55bb01e5e70f075f0aa31d7752993cc51b2bbd
OHHYUN <omplcie@gmail.com>
2021-05-26 23:45:09 +0900

    Test File init

--- src/test/java/study/datajpa/DataJpaApplicationTests.java
+++ src/test/java/study/datajpa/DataJpaApplicationTests.java
@@ -1,9 +1,7 @@
 package study.datajpa;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import study.datajpa.entity.Member;
 
 @SpringBootTest
 class DataJpaApplicationTests {
